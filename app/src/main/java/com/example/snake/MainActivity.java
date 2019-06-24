35f651fef68b915d41523351106fb798beb7a98b
kevin.thurnes <kevin.thurnes@gmail.com>
2019-06-24 10:18:17 +0200

    Added start screen delay 10 sec.

--- app/src/main/java/com/example/snake/MainActivity.java
+++ app/src/main/java/com/example/snake/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int WELCOME = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_end);
+        setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent startInten = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(startInten);
+                finish();
+            }
+        },WELCOME);
     }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_end);
+//    }
 }
