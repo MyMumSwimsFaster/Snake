1217cfe4597d4480a21658f62a7fa1c22fbed981
kevin.thurnes <kevin.thurnes@gmail.com>
2019-06-26 13:58:10 +0200

    Help button and help activity done.

---
+++ app/src/main/java/com/example/snake/HelpActivity.java
@@ -0,0 +1,28 @@
+package com.example.snake;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Button helpback = findViewById(R.id.helpBack);
+        helpback.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.helpBack:
+                Intent startInten = new Intent(HelpActivity.this.getApplicationContext(), PregameActivity.class);
+                startActivity(startInten);
+        }
+    }
+}
